06f99eca7580cd55e9aa26f44691b9ea1d7279b4
kishore548614 <k.kishore858@gmail.com>
2018-06-28 12:32:02 +0530

    added file

---
+++ ProducerThread.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package MultiThreading;
+
+/**
+ * @author Krishnakishore_Kollu
+ *
+ */
+public class ProducerThread {
+
+	/**
+	 * 
+	 */
+	public ProducerThread() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
